b99663faa61cce6f2af6d2bd53f3317ef62e79c4
sfurrow88 <sfurrow88@gmail.com>
2012-02-19 18:39:24 +0000

    Initial commit of Keil code

---
+++ Keil Code/MainFiles/i2cTemp.c
@@ -0,0 +1,183 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "projdefs.h"
+#include "semphr.h"
+
+/* include files. */
+#include "vtUtilities.h"
+#include "vtI2C.h"
+#include "LCDtask.h"
+#include "i2cTemp.h"
+
+
+
+// I have set this to a large stack size because of (a) using printf() and (b) the depth of function calls
+//   for some of the LCD operations
+#if PRINTF_VERSION==1
+#define i2cSTACK_SIZE		8*configMINIMAL_STACK_SIZE
+#else
+#define i2cSTACK_SIZE		4*configMINIMAL_STACK_SIZE
+#endif
+
+// Set the task up to run every second
+#define i2cREAD_RATE_BASE	( ( portTickType ) 1 )
+
+/* The i2cTemp task. */
+static portTASK_FUNCTION_PROTO( vi2cTempUpdateTask, pvParameters );
+
+/*-----------------------------------------------------------*/
+
+void vStarti2cTempTask( unsigned portBASE_TYPE uxPriority, i2cTempStruct *params)
+{
+	/* Start the task */
+	portBASE_TYPE retval;
+
+	if ((retval = xTaskCreate( vi2cTempUpdateTask, ( signed char * ) "i2cTemp", i2cSTACK_SIZE, (void *) params, uxPriority, ( xTaskHandle * ) NULL )) != pdPASS) {
+		VT_HANDLE_FATAL_ERROR(retval);
+	}
+}
+
+// This is the actual task that is run
+static portTASK_FUNCTION( vi2cTempUpdateTask, pvParameters )
+{
+	portTickType xUpdateRate, xLastUpdateTime;
+	const uint8_t i2cCmdInit[]= {0xAC,0x00};
+	const uint8_t i2cCmdStartConvert[]= {0xEE};
+	const uint8_t i2cCmdStopConvert[]= {0x22};
+	const uint8_t i2cCmdReadVals[]= {0xAA};
+	const uint8_t i2cCmdReadCnt[]= {0xA8};
+	const uint8_t i2cCmdReadSlope[]= {0xA9};
+	float temperature;
+	float countPerC, countRemain;
+	uint8_t messageReceived[2];
+	uint8_t temp1, rxLen, status;
+	// Get the parameters
+	i2cTempStruct *param = (i2cTempStruct *) pvParameters;
+	// Get the I2C device pointer
+	vtI2CStruct *devPtr = param->dev;
+	// Get the LCD information pointer
+	vtLCDStruct *lcdData = param->lcdData;
+	vtLCDMsg lcdBuffer;
+
+	// Assumes that the I2C device (and thread) have already been initialized
+	// 0x4F address of the temperature sensor (DS1621)
+	// Should do something more comprehensive with the msgType for debugging...
+
+	// Temperature sensor configuration sequence (DS1621)
+	if (vtI2CEnQ(devPtr,0x01,0x4F,sizeof(i2cCmdInit),i2cCmdInit,0) != pdTRUE) {
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	// wait on the result
+	if (vtI2CDeQ(devPtr,0,NULL,&rxLen,&status) != pdTRUE) {
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	// Must wait 10ms after writing to the temperature sensor's configuration registers(per sensor data sheet)
+	vTaskDelay(10/portTICK_RATE_MS);
+	// Tell it to start converting
+	if (vtI2CEnQ(devPtr,0x02,0x4F,sizeof(i2cCmdStartConvert),i2cCmdStartConvert,0) != pdTRUE) {
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	// wait on the result
+	if (vtI2CDeQ(devPtr,0,NULL,&rxLen,&status) != pdTRUE) {
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+
+	// Scale the update rate to ensure it really is in ms
+	xUpdateRate = i2cREAD_RATE_BASE / portTICK_RATE_MS;
+
+	/* We need to initialise xLastUpdateTime prior to the first call to vTaskDelayUntil(). */
+	xLastUpdateTime = xTaskGetTickCount();
+	
+	// Like all good tasks, this should never exit
+	for(;;)
+	{
+		/* Ask the RTOS to delay reschduling this task for the specified time */
+		vTaskDelayUntil( &xLastUpdateTime, xUpdateRate );
+		
+		// Read in the values from the temperature sensor
+		// We have three transactions on i2c to read the full temperature 
+		//   we send all three requests to the I2C thread (via a Queue) and *then* we wait on all three responses
+		// Temperature read -- use a convenient routine defined above
+		
+		
+		if (vtI2CEnQ(devPtr,0x01,0x4F,sizeof(i2cCmdReadVals),i2cCmdReadVals,2) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
+
+
+		// Read in the read counter
+		if (vtI2CEnQ(devPtr,0x04,0x4F,sizeof(i2cCmdReadCnt),i2cCmdReadCnt,1) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
+		// Read in the slope;
+		if (vtI2CEnQ(devPtr,0x05,0x4F,sizeof(i2cCmdReadSlope),i2cCmdReadSlope,1) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
+		// wait on the results for the first read (ignore the second byte because we are doing a special computation -- see the sensor datasheet)
+		
+		
+		if (vtI2CDeQ(devPtr,2,&messageReceived[0],&rxLen,&status) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
+
+		
+
+		temperature = messageReceived[0];//<<8;
+
+
+		//temperature = temperature + messageReceived[1];
+		//temperature = temp1;
+			
+		vtITMu8(vtITMPortTempVals,rxLen); // Log the length received
+		// wait on the results of the second read
+		if (vtI2CDeQ(devPtr,1,&temp1,&rxLen,&status) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
+		countRemain = temp1;
+		vtITMu8(vtITMPortTempVals,rxLen); // Log the length received;
+		// wait on the results of the third read
+		if (vtI2CDeQ(devPtr,1,&temp1,&rxLen,&status) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
+		countPerC = temp1;	
+		vtITMu8(vtITMPortTempVals,rxLen); // Log the length received;
+		// end of i2c temperature read
+
+		// Do the accurate temperature calculation
+		temperature += -0.25 + ((countPerC-countRemain)/countPerC);
+		
+		//#if PRINTF_VERSION == 1
+		printf("Temp %f F (%f C)\n",(32.0 + ((9.0/5.0)*temperature)), (temperature));
+		
+		/*My Changes*/
+		lcdBuffer.buf[0] = messageReceived[0];
+		lcdBuffer.buf[1] = messageReceived[1];
+		//sprintf((char*)(lcdBuffer.buf),"%i",messageReceived[0]);
+	   	
+
+
+		/*End My Changes*/
+		//#else
+		// we do not have full printf (so no %f) and therefore need to print out integers
+		printf("Temp %d F (%d C)\n",lrint(32.0 + ((9.0/5.0)*temperature)), lrint(temperature));
+		sprintf((char*)(lcdBuffer.buf),"(%d C)",lrint(temperature));
+		//#endif
+			 
+		if (lcdData != NULL) {
+			// Send a message to the LCD task for it to print (and the LCD task must be configured to receive this message)
+			lcdBuffer.length = strlen((char*)(lcdBuffer.buf))+1;
+			
+
+			if (xQueueSend(lcdData->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+				VT_HANDLE_FATAL_ERROR(0);
+			}
+		}
+	}
+}
+
---
+++ Keil Code/test.c
@@ -0,0 +1,166 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "projdefs.h"
+#include "semphr.h"
+
+/* include files. */
+#include "vtUtilities.h"
+#include "vtI2C.h"
+#include "LCDtask.h"
+#include "i2cTemp.h"
+
+
+
+// I have set this to a large stack size because of (a) using printf() and (b) the depth of function calls
+//   for some of the LCD operations
+#if PRINTF_VERSION==1
+#define i2cSTACK_SIZE		8*configMINIMAL_STACK_SIZE
+#else
+#define i2cSTACK_SIZE		4*configMINIMAL_STACK_SIZE
+#endif
+
+// Set the task up to run every second
+#define i2cREAD_RATE_BASE	( ( portTickType ) 1 )
+
+/* The i2cTemp task. */
+static portTASK_FUNCTION_PROTO( I2CTask, pvParameters );
+
+/*-----------------------------------------------------------*/
+
+void vStartI2CTask( unsigned portBASE_TYPE uxPriority, i2cTempStruct *params)
+{
+	/* Start the task */
+	portBASE_TYPE retval;
+
+	if ((retval = xTaskCreate( I2CTask, ( signed char * ) "i2cTemp", i2cSTACK_SIZE, (void *) params, uxPriority, ( xTaskHandle * ) NULL )) != pdPASS) {
+		VT_HANDLE_FATAL_ERROR(retval);
+	}
+}
+
+// This is the actual task that is run
+static portTASK_FUNCTION( I2CTask, pvParameters )
+{
+	portTickType xUpdateRate, xLastUpdateTime;
+	const uint8_t ReturnADCValue = 0xAA;
+	const uint8_t Gimmesomething = 0xBB;
+	
+	uint8_t temp1, rxLen, status;
+	uint8_t ADCValueReceived[2];
+	uint8_t SecondaryReceived[2];
+
+
+	// Get the parameters
+	i2cTempStruct *param = (i2cTempStruct *) pvParameters;
+	
+	// Get the I2C device pointer
+	vtI2CStruct *devPtr = param->dev;
+	
+	// Get the LCD information pointer
+	vtLCDStruct *lcdData = param->lcdData;
+	vtLCDMsg lcdBuffer;
+
+	vTaskDelay(10/portTICK_RATE_MS);
+
+	xUpdateRate = i2cREAD_RATE_BASE / portTICK_RATE_MS;
+
+	/* We need to initialise xLastUpdateTime prior to the first call to vTaskDelayUntil(). */
+	xLastUpdateTime = xTaskGetTickCount();
+
+	int i = 0;
+	for(;;)
+	{
+		//delay for some amount of time before looping again
+		vTaskDelayUntil( &xLastUpdateTime, xUpdateRate );
+
+		//int i = 0;
+
+		//toggle the bottom LED
+		uint8_t ulCurrentState = GPIO2->FIOPIN;
+		if( ulCurrentState & 0x40 )
+		{
+			GPIO2->FIOCLR = 0x40;
+		}
+		else
+		{
+			GPIO2->FIOSET = 0x40;
+		}
+
+		//Ask for message from I2C
+		if (vtI2CEnQ(devPtr,0x01,0x40,1,&ReturnADCValue,2) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
+
+		//wait for message from I2C
+		if (vtI2CDeQ(devPtr,2,&ADCValueReceived[0],&rxLen,&status) != pdTRUE) {
+			//VT_HANDLE_FATAL_ERROR(0);
+		} 
+
+
+		if (vtI2CEnQ(devPtr,0x01,0x4F,1,&Gimmesomething,2) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
+
+		//wait for message from I2C
+		if (vtI2CDeQ(devPtr,2,&SecondaryReceived[0],&rxLen,&status) != pdTRUE) {
+			//VT_HANDLE_FATAL_ERROR(0);
+		}  
+
+		//uint8_t testint = 0;
+		
+		//load the read message from I2C into the lcd Buffer
+		lcdBuffer.buf[0] = ADCValueReceived[0];
+		lcdBuffer.buf[1] = ADCValueReceived[1];
+		
+		if (lcdBuffer.buf[0] == 0xFF)
+		{
+			uint8_t ulCurrentState = GPIO2->FIOPIN;
+			if( ulCurrentState & 0x20 )
+			{
+				GPIO2->FIOCLR = 0x20;
+			}
+			else
+			{
+				GPIO2->FIOSET = 0x20;
+			}
+		}
+		if (lcdData != NULL && lcdBuffer.buf[0] != 0xFF) {
+			// Send a message to the LCD task for it to print (and the LCD task must be configured to receive this message)
+			//lcdBuffer.length = strlen((char*)(lcdBuffer.buf))+1;
+			
+			uint8_t ulCurrentState = GPIO2->FIOPIN;
+			if( ulCurrentState & 0x10 )
+			{
+				GPIO2->FIOCLR = 0x10;
+			}
+			else
+			{
+				GPIO2->FIOSET = 0x10;
+			}
+
+			i++;
+			if (i > 142)
+			{
+			 	uint8_t ulCurrentState = GPIO2->FIOPIN;
+				if( ulCurrentState & 0x08 )
+				{
+					GPIO2->FIOCLR = 0x08;
+				}
+				else
+				{
+					GPIO2->FIOSET = 0x08;
+				}
+				i = 0;
+			}
+			/*if (xQueueSend(lcdData->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+				VT_HANDLE_FATAL_ERROR(0);
+			} */
+		}
+	}
+}
+
