412a95d59a7d194be5282c1fa3e128241e6b47cd
taekwonjoe01@gmail.com <taekwonjoe01@gmail.com@8f1943a5-2e38-0283-803f-93829b7e21d4>
2012-04-19 00:40:54 +0000

    Added test cases

--- RTOSDemo/joystick.c
+++ RTOSDemo/joystick.c
@@ -15,6 +15,10 @@
 #include "LCDtask.h"
 #include "i2cTemp.h"
 
+#define test 0
+
+//hello world
+
 
 
 #define ADC_POWERON (1 << 12) 
@@ -40,51 +44,6 @@ void main_delay(int n)
    for (d=0; d<n*3000; d++){}
 }
 
-
-/*int main(void) {
-
-	volatile static int i = 0 ;
-	int adval;
-	
-	// Set up board/processor
-	TargetResetInit();
-
-	// Turn on power to ADC block 
-	PCONP |=  ADC_POWERON;
-
-	// Turn on ADC peripheral clock
-	PCLKSEL0 = PCLKSEL0 & ~(PCLK_ADC_MASK);
-	PCLKSEL0 |=  (3 << PCLK_ADC);
-		
-	// Set P0.23 to AD0.0 in PINSEL1
-	PINSEL1	|= SELECT_ADC0; 
-	
-	// Enter an infinite loop, just checking ADC pot and incrementing a counter
-	while(1) {
-
-		// Start A/D conversion for on AD0.0
-		AD0CR = START_ADC | OPERATIONAL_ADC | SEL_AD0 ;
-
-		do {
-			adval = AD0GDR;                      // Read A/D Data Register
-		} while ((adval & ADC_DONE_BIT) == 0);   // Wait for end of A/D Conversion
-		
-		AD0CR &= ~(START_ADC | OPERATIONAL_ADC | SEL_AD0 );   // Stop A/D Conversion
-
-		 // Extract AD0.0 value - 12 bit result in bits [15:4]
-		adval = (adval >> 4) & 0x0FFF ;            
-
-		printf ("%d - Pot val = %d\n", i, adval);
-		main_delay(5000);	//Short delay before doing another ADC read
-		i++ ;
-	}
-	return 0 ;
-}*/
-
-
-
-
-
 // I have set this to a large stack size because of (a) using printf() and (b) the depth of function calls
 //   for some of the LCD operations
 
@@ -107,6 +66,7 @@ void vStartJoystickTask( unsigned portBASE_TYPE uxPriority, i2cParamStruct *para
 	}
 }
 
+
 // This is the actual task that is run
 static portTASK_FUNCTION( JoystickTask, pvParameters )
 {
@@ -118,12 +78,366 @@ static portTASK_FUNCTION( JoystickTask, pvParameters )
 
 	uint8_t i = 0;
 	int count = 0;
+	printf("Start of Joystick thread. Begin for loop.\n");
+
+	vTaskDelay(5000);
 
 	for(;;)
 	{
 		PIN_CONFIG = LPC_GPIO1->FIOPIN >>20;
 		//test = test * -1;
-		 
+#if test==1
+	//Volume change test
+	printf("Volume change test\n");
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 2;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+
+	vTaskDelay(10000);
+
+	printf("Brightness change test\n");
+	//Brightness Change test
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 2;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);	
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);	
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);	
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 4;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+
+	vTaskDelay(10000);
+
+	//Player 1 Instrument change test
+	printf("Player 1 instrument change test\n");
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 2;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);	
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);	
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);	
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);	
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);	
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);	
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+
+	vTaskDelay(10000);
+
+	//Repeating Instrument Note Change
+	printf("Repeating Instrument Note change test\n");
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 1;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+
+	vTaskDelay(10000);
+
+	//Repeating Instrument BPM change dependent upon above test
+	printf("Repeating Instrument BPM change test\n");
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 1;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 1;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 1;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 1;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+
+	vTaskDelay(10000);
+
+	//Final test to demonstrate working Repeating Instrument generation
+	printf("Repeating Instrument instrument change test\n");
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 3;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 1;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	lcdBuffer.length = 2;
+	lcdBuffer.buf[0] = 0x11;
+	lcdBuffer.buf[1] = 0;
+	if (xQueueSend(lcdQ->inQ,(void *) (&lcdBuffer),portMAX_DELAY) != pdTRUE) {  
+		VT_HANDLE_FATAL_ERROR(0);
+	}
+	vTaskDelay(50);
+	
+#else		 
 		if ((PIN_CONFIG & 0x1) == 0 && (PIN_CONFIG != PrevPIN_CONFIG))	 //select BIT
 		{
 			lcdBuffer.length = 2;
@@ -184,6 +498,7 @@ static portTASK_FUNCTION( JoystickTask, pvParameters )
 			
 		PrevPIN_CONFIG = PIN_CONFIG;
 		vTaskDelay(30);		//delay 30ms
+#endif
 	}
 }
 
