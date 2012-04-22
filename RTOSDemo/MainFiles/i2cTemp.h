f4e0240b5d89a1d7d447be94336b37b92be2d6c1
sfurrow88 <sfurrow88@gmail.com>
2012-04-22 19:31:12 +0000

    Removed RTOSDemo folder. If you absolutely need it back just update to an old revision, copy the folder then add it back.

--- RTOSDemo/MainFiles/i2cTemp.h
+++
@@ -1,8 +0,0 @@
-#ifndef I2CTEMP_TASK_H
-#define I2CTEMP_TASK_H
-
-#include "messagequeues.h"
-
-void vStarti2cTempTask( unsigned portBASE_TYPE uxPriority, i2cParamStruct *);
-
-#endif
--- RTOSDemo/MainThread.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAINTHREAD_H
-#define MAINTHREAD_H
-
-#include "messagequeues.h"
-
-void vStartMainThread( unsigned portBASE_TYPE uxPriority, MasterParamStruct *);
-
-#endif
