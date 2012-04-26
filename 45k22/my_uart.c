38cc9339183dbeb66eccdaee743a6f7133a81a0d
homes315 <homes315@gmail.com>
2012-04-26 04:02:46 +0000

    Insteon messages working

--- 45k22/my_uart.c
+++ 45k22/my_uart.c
@@ -7,24 +7,30 @@ static uart_comm *uc_ptr;
 void uart_recv_int_handler()
 {
 	if (DataRdy1USART()) {
+	
 		uc_ptr->buffer[uc_ptr->buflen] = Read1USART();
 		uc_ptr->buflen++;
+
 		// check if a message should be sent
-		if (uc_ptr->buflen == MAXUARTBUF) {
+		if (uc_ptr->buflen == MAXUARTBUF) 
+		{
+			
 			ToMainLow_sendmsg(uc_ptr->buflen,MSGT_UART_DATA,(void *) uc_ptr->buffer);
 			uc_ptr->buflen = 0;
 		}
 	}
-	if (USART1_Status.OVERRUN_ERROR == 1) {
+	if (RCSTA1bits.OERR == 1) {
 		// we've overrun the USART and must reset
 		// send an error message for this
+		//LATB = 0xFF;
 		RCSTAbits.CREN = 0;
 		RCSTAbits.CREN = 1;
-		ToMainLow_sendmsg(0,MSGT_OVERRUN,(void *) 0);
+		//ToMainLow_sendmsg(0,MSGT_OVERRUN,(void *) 0);
 	}
 }
 
 void init_uart_recv(uart_comm *uc)
-{	uc_ptr = uc;
+{	
+	uc_ptr = uc;
 	uc_ptr->buflen = 0;
 }
